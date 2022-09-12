2ebadc88072c910823b82f61093bb7d2e0864d81
Douppen <simon.nars@gmail.com>
2022-09-12 13:59:44 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+// CONNECT TO MONGODB
+logger.info("connecting to MongoDB");
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB:", error.message);
+  });
+
+// MIDDLEWARE PRECEDING ROUTES
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+// APP ROUTES
+app.use("/api/blogs", blogsRouter);
+
+// MIDDLEWARE SUCCEEDING ROUTES
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
