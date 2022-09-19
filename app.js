65ef18e24091b2dd77e896bcf2e061e6d8df98e5
Douppen <simon.nars@gmail.com>
2022-09-19 23:13:20 +0300

    bugfixes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose
 // MIDDLEWARE PRECEDING ROUTES
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
